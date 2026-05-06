af7a85661667aebe4f0bce4e8d06fe705f68a8ad
Chenthil Hari <chenthilhari@gmail.com>
2026-05-06 14:23:41 +0530

    Inject Tailwind CDN in layout.js to fix layout alignments globally

--- app/layout.js
+++ app/layout.js
@@ -32,6 +32,9 @@ import NeuralTransition from "./components/NeuralTransition";
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${outfit.variable} ${plusJakarta.variable}`} suppressHydrationWarning>
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
       <body>
         <Providers>
           <MaintenanceGuard>
